926aa1d98bb003d999c5a0a13e68371f009c2e52
Git-Xian831 <christian.k.horban@gmail.com>
2022-11-21 16:01:58 -0700

    favoriteFood
    Added favoriteFood var to .js file and called it with document.write

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
-let size = 100;
-let doubleSize = size * 2;
-let minSize = (doubleSize * 2) - (size / 2);
-document.write(minSize);
+alert('Hello world');
+
+let favoriteFood = 'spaghetti with mizithra cheese';
+
+document.write(favoriteFood);
