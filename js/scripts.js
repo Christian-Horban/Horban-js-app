ade757beef49f875246b6bceb956983ecf5ed0bf
Git-Xian831 <christian.k.horban@gmail.com>
2022-11-25 14:02:43 -0700

    Update scripts.js
    Added pokemonList array with 3 objects

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+let pokemonList = [
+   {name: 'Bulbasaur', height: 2, types: 'grass', 'poison'},
+   {name: 'Squirtle', height: 1, types: 'water'},
+   {name: 'Charmander', height: 2, types: 'fire'}
+];
