767f6253fe0aa45a196feec8c19a596f8784ebb2
Git-Xian831 <christian.k.horban@gmail.com>
2022-11-28 17:15:51 -0700

    Created for loop
    Created a for loop to display each object in the pokemonList array.
    
    added a conditional on the height value

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,18 @@
 let pokemonList = [
-   {name: 'Bulbasaur', height: 2, types: 'grass', 'poison'},
-   {name: 'Squirtle', height: 1, types: 'water'},
+   {name: 'Bulbasaur', height: 2.4, types: ['grass', ' poison',]},
+   {name: 'Squirtle', height: 1.8, types: 'water'},
    {name: 'Charmander', height: 2, types: 'fire'}
 ];
+
+//loop for printing pokemonList
+for (let i = 0;
+   i <pokemonList.length; i++) {
+      if (pokemonList[i].height >2) {
+         document.write(pokemonList[i].name + " is " + pokemonList[i].height + " feet tall and it is a " + pokemonList[i].types + " type! Wow, that's big!<br>")
+
+      }
+      else {
+         document.write(pokemonList[i].name + " is " + pokemonList[i].height + " feet tall and it is a " + pokemonList[i].types + " type!<br>")
+
+      }
+   }
